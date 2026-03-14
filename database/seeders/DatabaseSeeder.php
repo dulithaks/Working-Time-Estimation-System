75acee81f7f65f0f7a67dd45e3d04ef877bb4fa0
Dulitha <dulithaks@gmail.com>
2026-03-14 23:10:00 +0530

    create holiday seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,5 +29,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(TaskSeeder::class);
         $this->call(WorkSettingsSeeder::class);
+        $this->call(HolidaySeeder::class);
     }
 }
